72a127ab3fe0edcf90208c3f9f0e31ab2f9597af
J. Ozymandias Fallick <ozymandiasthegreat@gmail.com>
2014-05-28 17:01:27 -0400

    Updated sidebar and Github handle

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $personal['email_address'] = 'ozzie.fallick@gmail.com';
 
 // Now let's add any social media pages you've created and want to share.  You can add whatever you want by following the format below.  The first item you add is the name of the social media site, and the second item you add is the URL to your page.
 $social_media[] = array('LinkedIn', 'http://www.linkedin.com/profile/view?id=99484667');
-$social_media[] = array('Github', 'http://www.github.com/ozymandiasthegreat');
+$social_media[] = array('Github', 'http://www.github.com/ozziefallick');
 
 // Cool.  With that stuff out of the way, let's input your work history.  This part requires a bit more information but it's similar to adding a social media page like you did above but with more information.  The first item you enter is last year you worked at the job (or the range of time, if you prefer).  The second item is your job title, the third is the company you worked for, and the fourth is a description of the work you did there.  The description can get a little long, but that's okay.  Just be sure not to use any single quotes ('') or things will break!  You can add as many jobs as you like and the script will adapt.
 $work_history[] = array('2011-',
